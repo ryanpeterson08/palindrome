223cf8714cee5c53e7e4a5f2d83569a61d10b2ce
Ryan Peterson and Rafael Perez <ryan@>
2016-10-25 16:10:34 -0700

    add bootstrap styles

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+
+var palindrome = function(text){
+
+	var textArray = text.split("");
+  var noSpaces = textArray.filter(function (value){
+  	return value != " ";
+  });
+
+  var flipArray = noSpaces.slice().reverse();
+
+  for(var x = 0; x <= textArray.length - 1; x++){
+  	if(noSpaces[x] != flipArray[x]){
+    	return false;
+    }
+  }
+  return true
+}
+
+$(document).ready(function (){
+  $("#palindrome").submit(function(event){
+    if (palindrome($("#sentence").val())){
+      $("#answer").text("This is a palindrome");
+    }
+		else{
+			$("#answer").text("This is not a palindrome");
+		}
+		$("img").fadeIn('slow');
+		$("#thanks").fadeIn('slow');
+    event.preventDefault();
+  })
+
+});
